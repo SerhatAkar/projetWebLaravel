06d1a21b6e20a70701996d431be273ce7f699207
ThibaultC123 <thibault.caci@viacesi.fr>
2019-01-26 19:31:19 +0100

    update boutique

--- resources/views/boutique.blade.php
+++ resources/views/boutique.blade.php
@@ -16,6 +16,24 @@
                         <a href="#" class="button">Promotions</a>
                     </div>
 
+                    <div class="row">
+                        <div class="offset-1 col-4-md" >
+                            <label style="padding-top: 6px "  for="filterValue">Filtrer:</label>
+                                <select class="form-control " id="filterValue" name="filterValue" onchange="filter(this.value)">
+                                    <option value="0">Tout</option>
+                                    <option value="1">Vêtements</option>
+                                    <option value="2">Goodies</option>
+                                    <option value="3">Divers</option>
+                                </select>
+                    </div>
+                        <div class="offset-2 col-3-md">
+                            <nav class="navbar navbar-light bg-light mb-3">
+                                <label style="padding-top: 6px ">Rechercher :</label>
+                                <input onchange="actionForm(this.value)" class="form-control mr-sm-2" id="search" type="search" placeholder="" aria-label="Search">
+                            </nav>
+                    {{--<input id="search" class="form-control" type="search">--}}
+                    </div>
+
                 </div> <!-- end container -->
 
             </div> <!-- end boutique-section -->
