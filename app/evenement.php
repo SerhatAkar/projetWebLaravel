35dbdec10d42de6100d1965074c159ced5bfd56d
LoriMoulin <37933646+LoriMoulin@users.noreply.github.com>
2019-01-28 11:41:43 +0100

    dz

--- app/evenement.php
+++ app/evenement.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class evenement extends Model
 {
-    //
+    use Notifiable;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * 
+     * @var array
+     */
+    protected $fillable = [
+         'email', 'password', 'lname','fname','username'
+    ];
 }
