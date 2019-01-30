c683dfb6b56998f203b735f7386b11cd52a10eaf
LoriMoulin <lori.moulin@viacesi.fr>
2019-01-30 00:51:18 -0800

    seeder update

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,31 +12,31 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         $users = new \App\User([
-            'fname' => '1',
-            'lname' => '1',
-            'username' => '1',
-            'email' => '1@gmail.com',
-            'password' => '1',
+            'fname' => 'student',
+            'lname' => 'student',
+            'username' => 'student',
+            'email' => 'student@viacesi.fr',
+            'password' => 'student',
             'permissions' => '0',
         ]);
            $users->save();
            
            $users = new \App\User([
-            'fname' => '2',
-            'lname' => '2',
-            'username' => '2',
-            'email' => '2@gmail.com',
-            'password' => '2',
+            'fname' => 'bde',
+            'lname' => 'bde',
+            'username' => 'bde',
+            'email' => 'bde@viacesi.fr',
+            'password' => 'bde',
             'permissions' => '1',
         ]);
            $users->save();
 
            $users = new \App\User([
-            'fname' => '3',
-            'lname' => '3',
-            'username' => '3',
-            'email' => '3@gmail.com',
-            'password' => '3',
+            'fname' => 'cesi',
+            'lname' => 'cesi',
+            'username' => 'cesi',
+            'email' => 'cesi@viacesi.fr',
+            'password' => 'cesi',
             'permissions' => '2',
         ]);
            $users->save();
